69e90dba99d8a3aab2dc7bf633311a3991752b84
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2022-08-21 18:43:46 -0300

    initial test

--- tests/unit/test_model_auth.py
+++ tests/unit/test_model_auth.py
@@ -3,11 +3,10 @@ from models.auth import AuthModel
 def test_model_new_auth():
 
     """
-    GIVE modeLs AuthModel
-    WHEN created a new user of authentication
+    GIVE modelo AuthModel
+    WHEN Cria um novo usuario de autenticação
     THEN Cheque o username, password e mostre os definidos corretamente
     """
-
 
     user = AuthModel('testmaster', 'paasswordtest')
     assert user.username == 'testmaster'
